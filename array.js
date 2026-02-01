cf68069d1ecc04e53bd088312fc5aad8f6d75836
yash-crypto-b <142340789+yash-crypto-b@users.noreply.github.com>
2026-02-01 15:34:33 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,19 @@
+let teaflavours = [
+  "Black Tea",
+  "Green Tea",
+  "Oolong Tea",
+  "White Tea",
+  "Pu-erh Tea",
+  "Herbal Tea",
+];
+console.log(teaflavours); 
+ const firstTeaFlavour = teaflavours[0];
+    console.log(firstTeaFlavour);
+    let favtea = ["herbal tea","white tea","green tea"];
+    console.log(favtea);
+    const jasmineTea = favtea[1];
+    console.log(jasmineTea);
+    let herfavtea = ["chai","earl grey","lemon tea","matcha tea","peppermint tea"];
+    herfavtea.pop();
+    console.log(herfavtea);
+    
