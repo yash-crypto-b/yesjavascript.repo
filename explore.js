ce535bcfb7fc3987f37feab1c6ee196c5d432fd6
yash-crypto-b <142340789+yash-crypto-b@users.noreply.github.com>
2026-02-02 19:20:51 +0530

    Add files via upload

---
+++ explore.js
@@ -0,0 +1,19 @@
+function sayHello(){
+    console.log("i would like to say hello !! ");
+
+} 
+setTimeout(()=>{
+ sayHello();
+
+},4000);
+for(let i = 0; i<10; i++){
+    console.log(i);
+
+}
+
+
+
+
+
+
+
