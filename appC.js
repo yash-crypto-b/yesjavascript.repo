9ab18c304b711d60d691f5fd973d6ea85eec6e3c
yash-crypto-b <142340789+yash-crypto-b@users.noreply.github.com>
2026-02-02 21:52:08 +0530

    Add files via upload

---
+++ appC.js
@@ -0,0 +1,2 @@
+const mathoperation = require("./mathop2.js");
+console.log(mathoperation.add(5,6));
---
+++ mathoperations.js
@@ -0,0 +1,12 @@
+export function add(a,b){
+ return  a+b;
+}
+export function substract(a,b) {
+return a-b;
+}
+export  default function multiply(a,b){
+    return a*b;
+
+
+}
+
